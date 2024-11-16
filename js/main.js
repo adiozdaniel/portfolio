123e3903139b70528f3e76e0327847b95ef904fd
adiozdaniel <adiozdaniel@gmail.com>
2024-11-16 16:04:52 +0300

    chores:add more banner images

--- js/main.js
+++ js/main.js
@@ -135,7 +135,7 @@ class BannerManager {
 }
 
 document.addEventListener("DOMContentLoaded", () => {
-	new BannerManager(".banner-container", 6);
+	new BannerManager(".banner-container", 7);
 
 	document.querySelectorAll(".engineer").forEach((engineer) => {
 		new EngineerScrollHandler(engineer);
